7a7eca93e284a1bfcf4c62b682d9121594227cf3
rojadhanavath-bpas <roja.dhanavath@bpaservices.com>
2018-05-25 21:37:25 -0400

    bootstrap for the project

---
+++ brauch/src/main/java/com/brauch/brauch/BrauchApplication.java
@@ -0,0 +1,12 @@
+package com.brauch.brauch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrauchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrauchApplication.class, args);
+	}
+}
