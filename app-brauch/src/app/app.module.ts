17e8413c132b098e078144868f8ac9e23d994a60
Roja Dhanavath <roja.d293@gmail.com>
2018-05-26 00:15:59 -0400

    updated bootstrap packages

--- app-brauch/src/app/app.module.ts
+++ app-brauch/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,MatButtonModule, MatCheckboxModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
