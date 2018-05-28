7df51c155730cab0734b3f922d080dba62e9f3f6
Roja Dhanavath <roja.d293@gmail.com>
2018-05-28 16:03:29 -0400

    cleanup - working version angular

--- app-brauch/src/app/app.module.ts
+++ app-brauch/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,MatButtonModule, MatCheckboxModule,
-    BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
