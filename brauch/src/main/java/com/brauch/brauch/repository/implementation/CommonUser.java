509454b6b8090f6416180010c9079fd08135046f
Roja Dhanavath <roja.d293@gmail.com>
2018-05-28 21:59:09 -0400

    Added Basic user and project folder structure.

---
+++ brauch/src/main/java/com/brauch/brauch/repository/implementation/CommonUser.java
@@ -0,0 +1,21 @@
+package com.brauch.brauch.repository.implementation;
+
+import com.brauch.brauch.repository.dao.IUserDao;
+
+public class CommonUser implements IUserDao {
+
+    @Override
+    public String getRights() {
+        return null;
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+}
